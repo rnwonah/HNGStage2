9e865502215872c431cc9b6ca257b0453b06037c
Richard Nwonah <rnwonah@outlook.com>
2018-04-14 08:15:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<Doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Richard Nwonah</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body style="position: relative; 
+    background: #000000;">
+    <h1 style="position: absolute;
+            width: 731px;
+            height: 113px;
+            left: 304px;
+            top: 100px;
+            font-family: Roboto;
+            font-style: 
+            normal;font-weight: 
+            normal;line-height: 
+            normal;
+            font-size: 96px;
+            color: #FFFFFF;">
+        HNG Internship 4
+    </h1>  
+
+    <p style="position: absolute;
+            width: 252px;
+            height: 43px;
+            left: 586px;
+            top: 280px;
+            font-family: Roboto;
+            font-style: normal;
+            font-weight: normal;
+            line-height: normal;
+            font-size: 36px;        
+            color: #FFFFFF;">
+        <?php
+            echo date("Y-m-d");
+        ?>
+    </p>  
+    <p style="position: absolute;
+        width: 171px;
+        height: 43px;
+        left: 584px;
+        top: 340px;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        line-height: normal;
+        font-size: 36px;
+        color: #FFFFFF;">
+    <?php
+        echo date("h:i:sa");
+    ?>
+    </p>
+</body>
+</html>
